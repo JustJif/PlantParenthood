2b855c75f909d4d6420652d52370700e3391a92b
Austen Tankersley <83746846+austentank@users.noreply.github.com>
2023-03-29 11:53:39 -0500

    Edit QRCodeGenerator

--- app/src/main/java/com/example/plantparenthood/QRCodeGenerator.java
+++ app/src/main/java/com/example/plantparenthood/QRCodeGenerator.java
@@ -1,14 +1,15 @@
 package com.example.plantparenthood;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.print.PrintHelper;
 
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import android.os.Bundle;
-import android.widget.NumberPicker;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
@@ -18,8 +19,8 @@ import com.google.zxing.qrcode.QRCodeWriter;
 public class QRCodeGenerator extends AppCompatActivity {
     private String qrData = "-1";
 
-    public void setData(String qrData){
-        this.qrData = qrData;
+    public void setData(int qrDataIn){
+        this.qrData = Integer.toString(qrDataIn);
     }
 
     @Override
@@ -30,19 +31,20 @@ public class QRCodeGenerator extends AppCompatActivity {
 
         //get references to the generate button and QR code image views.
         Button generateButton = findViewById(R.id.qrcode_generate_button);
+        Button printButton = findViewById(R.id.qrcode_print_button);
         ImageView qrCodeImage = findViewById(R.id.qrcode_image);
         EditText debugNumberEntry = findViewById(R.id.debug_plantIDEntry);
 
-        //set a click listener on the generate button.
+        //set a click listener on the generate button. Generates the QR code and replaces qrCodeImage with the generated code.
         generateButton.setOnClickListener(v -> {
             //generate a QR code and set it to the image view.
-            String plantIDString = "-1";
             //get the text from debugNumberEntry to turn into a QR code.
-            plantIDString = debugNumberEntry.getText().toString();
+            //eventually delete this when setQRdata is implemented elsewhere.
+            qrData = (debugNumberEntry.getText().toString());
             int size = qrCodeImage.getWidth();
             QRCodeWriter writer = new QRCodeWriter();
             try{
-                BitMatrix bitMatrix = writer.encode(plantIDString, BarcodeFormat.QR_CODE, size, size);
+                BitMatrix bitMatrix = writer.encode(qrData, BarcodeFormat.QR_CODE, size, size);
                 int[] pixels = new int[size*size];
                 for(int y = 0; y<size; y++){
                     int offset = y*size;
@@ -59,6 +61,13 @@ public class QRCodeGenerator extends AppCompatActivity {
 
         });
 
-        return;
+        //print the qr code on button click
+        printButton.setOnClickListener(v ->{
+            PrintHelper printer = new PrintHelper(this);
+            printer.setScaleMode(PrintHelper.SCALE_MODE_FIT);
+            Bitmap bitmap = ((BitmapDrawable) qrCodeImage.getDrawable()).getBitmap();
+            printer.printBitmap("qrcode", bitmap);
+        });
+
     }
 }
