5a19da00515900e1e4ef6248d6d452e48cab0db3
Austen Tankersley <83746846+austentank@users.noreply.github.com>
2023-04-10 17:52:57 -0500

    Rename files

--- app/src/main/java/com/example/plantparenthood/MainActivity.java
+++ app/src/main/java/com/example/plantparenthood/MainActivity.java
@@ -2,24 +2,13 @@ package com.example.plantparenthood;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.plantparenthood.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
