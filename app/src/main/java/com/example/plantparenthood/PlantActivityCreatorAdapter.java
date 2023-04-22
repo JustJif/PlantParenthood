6ace47e03a6ef6a59de7fa4e5b3a9f5ec59b2c5c
JustJif <65685934+JustJif@users.noreply.github.com>
2023-04-22 12:49:46 -0500

    Camera support for update plant has been added

--- app/src/main/java/com/example/plantparenthood/PlantActivityCreatorAdapter.java
+++ app/src/main/java/com/example/plantparenthood/PlantActivityCreatorAdapter.java
@@ -1,10 +1,16 @@
 package com.example.plantparenthood;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -20,11 +26,14 @@ import java.util.List;
 public class PlantActivityCreatorAdapter extends RecyclerView.Adapter
 {
     private List<Plant> plantsList;
+    private Plant_Activity plant_activity;
     private Context whatContext;
-    public PlantActivityCreatorAdapter(List<Plant> newPlantsList, Context newContext)
+    private ImageView plantImage;
+    public PlantActivityCreatorAdapter(List<Plant> newPlantsList, Plant_Activity plant_activity)
     {
         plantsList = newPlantsList;
-        whatContext = newContext;
+        this.plant_activity = plant_activity;
+        whatContext = plant_activity;
     }
 
     @NonNull
@@ -41,7 +50,7 @@ public class PlantActivityCreatorAdapter extends RecyclerView.Adapter
     {
         Plant thisPlant = plantsList.get(position);
         TextView plantCommonName = (TextView) holder.itemView.findViewById(R.id.plantCommonName);
-        ImageView plantImage = (ImageView) holder.itemView.findViewById(R.id.plantImage);
+        plantImage = (ImageView) holder.itemView.findViewById(R.id.plantImage);
 
         plantCommonName.setText(thisPlant.getCommon_name());
         plantImage.setImageBitmap(thisPlant.getDefault_image());
@@ -66,19 +75,13 @@ public class PlantActivityCreatorAdapter extends RecyclerView.Adapter
 
         TextView plantCommonName = newPopup.findViewById(R.id.plantCommonName);
         plantCommonName.setText(thisPlant.getCommon_name());
-        plantCommonName.setFocusableInTouchMode(false);
-        plantCommonName.setFocusable(false);
         plantCommonName.setEnabled(false);
-        plantCommonName.setInputType(0);
 
         TextView plantScientificName = newPopup.findViewById(R.id.plantScientificName);
         plantScientificName.setText(thisPlant.getScientific_name());
-        plantScientificName.setFocusableInTouchMode(false);
-        plantScientificName.setFocusable(false);
         plantScientificName.setEnabled(false);
-        plantScientificName.setInputType(0);
 
-        ImageView plantImage = newPopup.findViewById(R.id.plantImage);
+        plantImage = newPopup.findViewById(R.id.plantImage);
         plantImage.setImageBitmap(thisPlant.getDefault_image());
 
         Button qrButton = newPopup.findViewById(R.id.qrButton);
@@ -102,19 +105,16 @@ public class PlantActivityCreatorAdapter extends RecyclerView.Adapter
         ImageView editCommonName = newPopup.findViewById(R.id.editCommon);
         editCommonName.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                //Toast.makeText(view.getContext(), "Pressed edit", Toast.LENGTH_SHORT).show();
-                plantCommonName.setFocusableInTouchMode(true);
-                plantCommonName.setFocusable(true);
-                plantCommonName.setEnabled(true);
+            public void onClick(View click) {
+                modifyText(plantCommonName, view);
             }
         });
 
         ImageView editScientificName = newPopup.findViewById(R.id.editScientific);
         editScientificName.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(view.getContext(), "Pressed edit", Toast.LENGTH_SHORT).show();
+            public void onClick(View click) {
+                modifyText(plantScientificName, view);
             }
         });
 
@@ -122,15 +122,52 @@ public class PlantActivityCreatorAdapter extends RecyclerView.Adapter
         editCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(view.getContext(), "Pressed edit", Toast.LENGTH_SHORT).show();
+                plant_activity.openCamera();
             }
         });
 
         Button updatePlant = newPopup.findViewById(R.id.updatePlant);
         updatePlant.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Toast.makeText(view.getContext(), "Plant successfully updated", Toast.LENGTH_SHORT).show();
+                new AlertDialog.Builder(whatContext)
+                .setTitle("Confirm changes")
+                .setMessage("Changes will override previous information, this cannot be undone.")
+                .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int id) {
+                        Toast.makeText(view.getContext(), "Applied changes", Toast.LENGTH_SHORT).show();
+                        newPopupWindow.dismiss();
+                    }
+                })
+                .setNegativeButton("Revert", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int id) {
+                        Toast.makeText(view.getContext(), "Reverted changes", Toast.LENGTH_SHORT).show();
+                        newPopupWindow.dismiss();
+                    }
+                })
+                .show();
             }
         });
+    }
+
+    private void modifyText(TextView editableText, View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) whatContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if(!editableText.isEnabled())
+        {
+            editableText.setEnabled(true);
+            editableText.requestFocus();
+            inputMethodManager.showSoftInput(editableText, 1);
+        }
+        else
+        {
+            editableText.setEnabled(false);
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    public void setCameraPreview(Bitmap image)
+    {
+        plantImage.setImageBitmap(image);
     }
 }
--- app/src/main/java/com/example/plantparenthood/Plant_Activity.java
+++ app/src/main/java/com/example/plantparenthood/Plant_Activity.java
@@ -1,18 +1,31 @@
 package com.example.plantparenthood;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PackageManagerCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -27,67 +40,80 @@ public class Plant_Activity extends AppCompatActivity {
     List<Plant> plantList;
     RecyclerView plantGrid;
     PlantDatabase plantDB;
+    ActivityResultLauncher<Intent> camera;
+    PlantActivityCreatorAdapter plantAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plants);
         plantList = new ArrayList<>();
-        plantDB = Room.databaseBuilder(getApplicationContext(),PlantDatabase.class,"PlantDatabase").build();
+        plantDB = Room.databaseBuilder(getApplicationContext(), PlantDatabase.class, "PlantDatabase").build();
 
         plantGrid = findViewById(R.id.plant_recycler_view);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(), 1);
         plantGrid.setLayoutManager(gridLayoutManager);
 
-        AsyncTask.execute(new Runnable()
-        {
+        AsyncTask.execute(new Runnable() {
             @Override
-            public void run()
-            {
+            public void run() {
                 getPlantsFromDB(plantDB.dataAccessObject());
                 Handler handler = new Handler(Looper.getMainLooper());
                 handler.post(() -> createPlantGrid(plantGrid));
             }
         });
         addPlant = (CardView) findViewById(R.id.addplant);
-        addPlant.setOnClickListener(new View.OnClickListener()
-        {
+        addPlant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), PlantSearcher.class));
             }
         });
 
+        camera = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>()
+                {
+                    @Override
+                    public void onActivityResult(ActivityResult result)
+                    {
+                        if (result.getResultCode() == RESULT_OK)
+                        {
+                            System.out.println("Image found");
+                            Bitmap image = (Bitmap) result.getData().getExtras().get("data");
+                            plantAdapter.setCameraPreview(image);
+                        }
+                    }
+                }
+        );
+
         // Initialize and assign variable
-        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         // Set Home selected
         bottomNavigationView.setSelectedItemId(R.id.plants);
 
         // Perform item selected listener
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item)
-            {
-                switch(item.getItemId())
-                {
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
                     case R.id.spaces:
                         startActivity(new Intent(getApplicationContext(), Space_Activity.class));
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.plants:
                         return true;
                     case R.id.home:
-                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.calendar:
-                        startActivity(new Intent(getApplicationContext(),Calendar.class));
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext(), Calendar.class));
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.settings:
-                        startActivity(new Intent(getApplicationContext(),Settings.class));
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext(), Settings.class));
+                        overridePendingTransition(0, 0);
                         return true;
                 }
                 return false;
@@ -96,14 +122,11 @@ public class Plant_Activity extends AppCompatActivity {
     }
 
     @Override
-    protected void onResume()
-    {
+    protected void onResume() {
         super.onResume();
-        AsyncTask.execute(new Runnable()
-        {
+        AsyncTask.execute(new Runnable() {
             @Override
-            public void run()
-            {
+            public void run() {
                 getPlantsFromDB(plantDB.dataAccessObject());
                 Handler handler = new Handler(Looper.getMainLooper());
                 handler.post(() -> createPlantGrid(plantGrid));
@@ -111,19 +134,30 @@ public class Plant_Activity extends AppCompatActivity {
         });
     }
 
-    private void getPlantsFromDB(DataAccessObject dataAccessObject)
-    {
+    private void getPlantsFromDB(DataAccessObject dataAccessObject) {
         plantList = dataAccessObject.loadAllPlants();
     }
 
-    public void createPlantGrid(RecyclerView plantGrid)
-    {
+    public void createPlantGrid(RecyclerView plantGrid) {
         //temp to set default image
         for (int i = 0; i < plantList.size(); i++) {
             plantList.get(i).setDefault_image(BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.defaultimage));
         }
 
-        PlantActivityCreatorAdapter plantAdapter = new PlantActivityCreatorAdapter(plantList,Plant_Activity.this);
+        plantAdapter = new PlantActivityCreatorAdapter(plantList, this);
         plantGrid.setAdapter(plantAdapter);
+    }
+
+    public void openCamera()
+    {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
+        }
+        else
+        {
+            Intent cameraInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            camera.launch(cameraInt);
+        }
     }
 }
