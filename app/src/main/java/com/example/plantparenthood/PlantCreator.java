989b5cb49b5a336cc068597a3367a8e057a66c0c
JustJif <65685934+JustJif@users.noreply.github.com>
2023-04-30 17:03:33 -0500

    Added JUnit dependencies to gradle

--- app/src/main/java/com/example/plantparenthood/PlantCreator.java
+++ app/src/main/java/com/example/plantparenthood/PlantCreator.java
@@ -1,19 +1,16 @@
 package com.example.plantparenthood;
 
+import android.app.Application;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.text.TextUtils;
-import android.widget.ImageView;
-
-import androidx.room.Room;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class PlantCreator {
     public DataAccessObject database;
@@ -22,60 +19,70 @@ public class PlantCreator {
         database = newDatabase;
     }
 
-    public void addPlant(JSONObject nonparsedPlants, PlantSearcher makethisplantUI) throws JSONException {
+    public ArrayList<Plant> createPlant(JSONObject nonparsedPlants, Context applicationContext, PlantSearcher makethisplantUI) {
         ArrayList<Plant> createdPlantObjects = new ArrayList<>();
-        JSONArray plantsList = nonparsedPlants.getJSONArray("data");
 
-        for (int i = 0; i < plantsList.length(); i++) {
-            JSONObject currentPlant = plantsList.getJSONObject(i);
-            int id = currentPlant.getInt("id");
-            String common_name = currentPlant.getString("common_name");
-            JSONArray scientific_name = currentPlant.getJSONArray("scientific_name");
-            JSONArray other_name = currentPlant.getJSONArray("other_name");
-            String cycle = currentPlant.getString("cycle");
-            String watering = currentPlant.getString("watering");
-            JSONArray sunlight = currentPlant.getJSONArray("sunlight");
+        JSONArray plantsList = null;
+        try
+        {
+            plantsList = nonparsedPlants.getJSONArray("data");
 
-            JSONObject default_image = null;
-            String imageURL = "";
-            if(currentPlant.has("default_image") && !currentPlant.isNull("default_image")) {
-                default_image = currentPlant.getJSONObject("default_image");
-                imageURL = default_image.getString("original_url");
+            for (int i = 0; i < plantsList.length(); i++) {
+                JSONObject currentPlant = plantsList.getJSONObject(i);
+                int id = currentPlant.getInt("id");
+                String common_name = currentPlant.getString("common_name");
+                JSONArray scientific_name = currentPlant.getJSONArray("scientific_name");
+                JSONArray other_name = currentPlant.getJSONArray("other_name");
+                String cycle = currentPlant.getString("cycle");
+                String watering = currentPlant.getString("watering");
+                JSONArray sunlight = currentPlant.getJSONArray("sunlight");
+
+                JSONObject default_image = null;
+                String imageURL = "";
+                if(currentPlant.has("default_image") && !currentPlant.isNull("default_image")) {
+                    default_image = currentPlant.getJSONObject("default_image");
+                    imageURL = default_image.getString("original_url");
+                }
+
+                String[] plantScientificNames = new String[scientific_name.length()];
+                for (int j = 0; j < scientific_name.length(); j++) {
+                    plantScientificNames[j] = scientific_name.getString(j);
+                }
+
+                /*String[] plantOtherNames = new String[other_name.length()];
+                for (int k = 0; k < scientific_name.length(); k++)
+                {
+                    plantOtherNames[k] = other_name.getString(k);
+                }*/
+
+                String[] sunlightArray = new String[sunlight.length()];
+                for (int j = 0; j < sunlight.length(); j++) {
+                    sunlightArray[j] = sunlight.getString(j);
+                }
+
+                Plant newPlant = new Plant.PlantBuilder()
+                        .setId(id)
+                        .setCommon_name(common_name)
+                        .setScientific_name(plantScientificNames[0])
+                        //.setOther_name(plantOtherNames[0])
+                        .setCycle(cycle)
+                        .setWatering(watering)
+                        .setSunlight(sunlightArray[0])
+                        .setPlantImageURL(imageURL)
+                        .setDefault_image(BitmapFactory.decodeResource(applicationContext.getResources(), R.drawable.defaultimage))
+                        .buildPlant();
+
+                createdPlantObjects.add(newPlant);
             }
 
-            String[] plantScientificNames = new String[scientific_name.length()];
-            for (int j = 0; j < scientific_name.length(); j++) {
-                plantScientificNames[j] = scientific_name.getString(j);
-            }
-
-            /*String[] plantOtherNames = new String[other_name.length()];
-            for (int k = 0; k < scientific_name.length(); k++)
-            {
-                plantOtherNames[k] = other_name.getString(k);
-            }*/
-
-            String[] sunlightArray = new String[sunlight.length()];
-            for (int j = 0; j < sunlight.length(); j++) {
-                sunlightArray[j] = sunlight.getString(j);
-            }
-
-            Plant newPlant = new Plant.PlantBuilder()
-                    .setId(id).setCommon_name(common_name)
-                    .setScientific_name(plantScientificNames[0])
-                    //.setOther_name(plantOtherNames[0])
-                    .setCycle(cycle)
-                    .setWatering(watering)
-                    .setSunlight(sunlightArray[0])
-                    .setPlantImageURL(imageURL)
-                    .setDefault_image(BitmapFactory.decodeResource(makethisplantUI.getApplicationContext().getResources(), R.drawable.defaultimage))
-                    .buildPlant();
-
-            createdPlantObjects.add(newPlant);
+            Integer currentPage = nonparsedPlants.getInt("current_page");
+            Integer lastPage = nonparsedPlants.getInt("last_page");
+            if(makethisplantUI != null)
+                makethisplantUI.createPlantGrid(createdPlantObjects, currentPage, lastPage);
+        } catch (JSONException e) {
+            return null;
         }
-
-        Integer currentPage = nonparsedPlants.getInt("current_page");
-        Integer lastPage = nonparsedPlants.getInt("last_page");
-        makethisplantUI.createPlantGrid(createdPlantObjects, currentPage, lastPage);
+        return createdPlantObjects;
     }
 
     public void addCustomPlant() {
