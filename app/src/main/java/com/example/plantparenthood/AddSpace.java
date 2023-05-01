45d143e29dad3081e736982950346ef62d76bc0d
AllyDauck <apdauck@ilstu.edu>
2023-05-01 13:55:09 -0500

    junk

---
+++ app/src/main/java/com/example/plantparenthood/AddSpace.java
@@ -0,0 +1,23 @@
+package com.example.plantparenthood;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Database;
+import androidx.room.Room;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.List;
+
+public class AddSpace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_space);
+        EditText nameEditText = (EditText) findViewById(R.id.names);
+        String fullName = nameEditText.getText().toString();
+        Space newSpace = new Space(fullName);
+    }
+}
