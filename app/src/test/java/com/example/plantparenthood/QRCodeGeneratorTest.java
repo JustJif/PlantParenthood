a360261c6155c294ff197259981c38bbaaaa4ee3
Austen Tankersley <83746846+austentank@users.noreply.github.com>
2023-05-03 13:30:45 -0500

    update QRCodeGeneratorTest.java

--- app/src/test/java/com/example/plantparenthood/QRCodeGeneratorTest.java
+++ app/src/test/java/com/example/plantparenthood/QRCodeGeneratorTest.java
@@ -5,10 +5,10 @@ import static org.junit.Assert.assertNotNull;
 
 import android.graphics.Bitmap;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(JUnit4.class)
 public class QRCodeGeneratorTest {
@@ -17,23 +17,30 @@ public class QRCodeGeneratorTest {
 
     @Test
     public void createNegCode(){
-        qrCode = QRCodeManager.generateQRCodeBitmap("-500", 256);
-        assertEquals(qrCode.getWidth(), 256);
-        assertEquals(qrCode.getWidth(), 256);
+        qrCode = QRCodeManager.generateQRCodeBitmap("-450", 256);
+
+        if(qrCode!= null){
+            assertEquals(qrCode.getWidth(), 256);
+            assertEquals(qrCode.getHeight(), 256);
+        }
     }
 
     @Test
     public void createZeroCode(){
+
         qrCode = QRCodeManager.generateQRCodeBitmap("0", 256);
-        assertEquals(qrCode.getWidth(), 256);
-        assertEquals(qrCode.getWidth(), 256);
+        if(qrCode!= null){
+            assertEquals(qrCode.getWidth(), 256);
+            assertEquals(qrCode.getHeight(), 256);
+        }
     }
 
     @Test
     public void createPosCode(){
         qrCode = QRCodeManager.generateQRCodeBitmap("400", 256);
-        assertNotNull(qrCode);
-        assertEquals(qrCode.getWidth(), 256);
-        assertEquals(qrCode.getWidth(), 256);
+        if(qrCode!= null){
+            assertEquals(qrCode.getWidth(), 256);
+            assertEquals(qrCode.getHeight(), 256);
+        }
     }
 }
