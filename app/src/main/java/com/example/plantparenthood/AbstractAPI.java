a90c0d18d882a3a49b27f7ecafa55e4a5ecfa48a
AllyDauck <apdauck@ilstu.edu>
2023-05-05 22:35:42 -0500

    DELETE ALL

--- app/src/main/java/com/example/plantparenthood/AbstractAPI.java
+++
@@ -1,9 +0,0 @@
-package com.example.plantparenthood;
-
-import com.android.volley.RequestQueue;
-
-public abstract class AbstractAPI
-{
-    public abstract void queryAPI(RequestQueue queue, String queryParams, Integer page);
-    public abstract void queryImageAPI(RequestQueue queue, Plant plant, int plantLocation);
-}
